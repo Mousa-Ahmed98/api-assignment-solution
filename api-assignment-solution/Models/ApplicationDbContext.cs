6fce832d0277940fd297d8d8074c7d169a0b6e80
Mousa <54138093+Mousa-Ahmed98@users.noreply.github.com>
2024-03-31 07:41:09 +0200

    Add project files.

---
+++ api-assignment-solution/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api_assignment_solution.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ api-assignment-solution/Program.cs
@@ -0,0 +1,40 @@
+
+using api_assignment_solution.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_assignment_solution
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer();
+            });
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
